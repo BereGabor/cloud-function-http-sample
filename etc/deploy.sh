da61aab45a6abaaf6e9f750155df24390489afb3
Gábor Bereczki <gabor.bereczki@icellmobilsoft.hu>
2022-03-12 07:56:46 +0100

    fix deploy script and .gcloudignore
    fix deploy script and .gcloudignore

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -1,5 +1,5 @@
 rm cloud-http-java-sample.zip
 cd ..
 zip -r etc/cloud-http-java-sample.zip pom.xml src -x *.DS_*
-gcloud functions deploy  cloud-http-java-sample-deploytest --region=europe-west1 --ingress-settings=all --memory=256MB   --entry-point=com.bere.cloud.functions.httpsample --runtime=java11 --trigger-http --allow-unauthenticated
+gcloud functions deploy  cloud-http-java-sample-deploytest --region=europe-west1 --ingress-settings=all --memory=256MB   --entry-point=com.bere.cloud.functions.httpsample --runtime=java11 --trigger-http --allow-unauthenticated --project=mlff-sb --max-instances=1000 --min-instances=10
 cd etc
